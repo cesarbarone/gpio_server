7681f1fa17fb9c980662bede9b37ad4c459bea53
César Barone <cesar.bmcosta@gmail.com>
2014-05-07 21:41:30 +0000

    integração com pi gpio

--- server.js
+++ server.js
@@ -1,9 +1,40 @@
 var http = require('http');
 var qs = require('querystring');
-var gpio = require('gpio');
+var gpio = require('pi-gpio');
+
+turnLedOn = function() {
+  gpio.open(11, "output", function(err) {        // Open pin 16 for output
+    gpio.write(11, 1, function() {            // Set pin 16 high (1)
+      gpio.close(11);                        // Close pin 16
+    });
+  });
+}
+
+turnLedOff = function() {
+  gpio.open(11, "output", function(err) {        // Open pin 16 for output
+    gpio.write(11, 0, function() {            // Set pin 16 high (1)
+      gpio.close(11);                        // Close pin 16
+    });
+  });
+}
+
+executeCommand = function(command) {
+  console.log(command);
+  switch(command) {
+    case 'ligar led':
+      turnLedOn();
+      break;
+
+    case 'desligar led':
+      turnLedOff();
+      break;
+
+    default: 
+      //TODO
+  }
+}
 
 http.createServer(function (req, res) {
-
   if (req.method == 'POST') {
 
     var body = '';
@@ -11,17 +42,13 @@ http.createServer(function (req, res) {
       body += data;
     });
     req.on('end', function() {
-      var POST = qs.parse(body)
-      gpio.open(17, "output", function(err) {        // Open pin 16 for output
-        gpio.write(17, 1, function() {            // Set pin 16 high (1)
-          gpio.close(17);                        // Close pin 16
-        });
-      });
-      
+      var POST = qs.parse(body);
+      command = POST.command;
+      executeCommand(command);
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end(command + '\n');
     })
 
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
   } else {
   
   }
